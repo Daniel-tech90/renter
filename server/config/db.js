d3a3319cd81c8c2aa1bb23dce55929ab1c815b7f
diensh <dk001a1@gmail.com>
2026-04-11 17:38:38 +0530

    fix: add MONGO_URI check before connecting

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  const uri = process.env.MONGO_URI;
+  if (!uri) {
+    console.error('MONGO_URI is not defined in environment variables');
+    process.exit(1);
+  }
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(uri);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
